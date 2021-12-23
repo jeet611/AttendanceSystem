af30c9559a0378d4f3db4c74ce1cbec215982556
jeet2610 <jeetshah0@icloud.com>
2021-12-24 01:15:11 +0530

    Google Account Added , Firebase Connected , Login working , User Name Printing , Date and Time Working

--- app/src/main/java/com/jams/attendencesystem/EmployeeDashboard.java
+++ app/src/main/java/com/jams/attendencesystem/EmployeeDashboard.java
@@ -1,14 +1,66 @@
 package com.jams.attendencesystem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class EmployeeDashboard extends AppCompatActivity {
+    TextView UserName;
+   // FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference databaseReference;
+    FirebaseUser user;
+    String uid;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employee_dashboard);
+
+        Calendar calendar = Calendar.getInstance();
+        String currentdate = DateFormat.getDateInstance(DateFormat.FULL).format(calendar.getTime());
+
+        TextView DateTextView = findViewById(R.id.DateTextview);
+        DateTextView.setText(currentdate);
+        TextView currentTime = findViewById(R.id.timeTextview);
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm:ss a");
+        String DateTime = simpleDateFormat.format(calendar.getTime());
+        currentTime.setText(DateTime);
+        UserName = findViewById(R.id.UserNameTextView);
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        uid = user.getUid();
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+    public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                    String user_name = snapshot.child(uid).child("UserName").getValue(String.class);
+
+                Toast.makeText(getApplicationContext(),"Welcome :"+ user_name , Toast.LENGTH_LONG).show();
+                UserName.setText("Hello" + user_name);
+                }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/jams/attendencesystem/EmployeeLogin.java
+++ app/src/main/java/com/jams/attendencesystem/EmployeeLogin.java
@@ -1,5 +1,6 @@
 package com.jams.attendencesystem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -11,27 +12,63 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class EmployeeLogin extends AppCompatActivity {
     EditText Email,Password;
+    FirebaseAuth auth;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+
         setContentView(R.layout.activity_employee_login);
         Email = findViewById(R.id.EmployeeEmailEditText);
         Password = findViewById(R.id.EmployeePasswordEditText);
         Button LoginBtn = findViewById(R.id.LoginEmployeeButton);
+        auth = FirebaseAuth.getInstance();
 
-        String EmailValue = Email.getText().toString();
-        String PasswordValue = Password.getText().toString();
         LoginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), EmployeeDashboard.class);
-                startActivity(intent);
+                LoginUser();
+                /*Intent intent = new Intent(getApplicationContext(), EmployeeDashboard.class);
+                startActivity(intent);*/
             }
         });
 
 
 
+    }
+
+    private void LoginUser() {
+        auth.signInWithEmailAndPassword(Email.getText().toString(),Password.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful())
+                        {
+                            FirebaseUser LoginUser = auth.getCurrentUser();
+                          //  Toast.makeText(getApplicationContext(),LoginUser+"LOGIN",Toast.LENGTH_LONG).show();
+                            Intent intent = new Intent(getApplicationContext(), EmployeeDashboard.class);
+                            startActivity(intent);
+
+                        }
+                       else
+                        {
+                            Toast.makeText(getApplicationContext(),"Incorrect User",Toast.LENGTH_LONG).show();
+                        }
+
+                    }
+                });
     }
 }
